47ecba276ca4a5d20efe27e913c7294ad15caafc
lupeMorales <morales.lupe@gmail.com>
2022-11-11 11:10:40 +0100

    add mui components

--- src/pages/jokeGenerator.jsx
+++ src/pages/jokeGenerator.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import Badge from "@mui/material/Badge";
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+
 import chuckNorris from "../images/chuckNorris-removebg-preview.png";
 import "../styles/pages/jokeGenerator.scss";
+
 const JokeGenerator = () => {
   return (
     <main className="chuck__container">
@@ -10,7 +15,15 @@ const JokeGenerator = () => {
         <p className="chuck__text">"Chuck Norris no duerme, espera"</p>
       </section>
 
-      <button className="chuck__button">generate new joke</button>
+      <section className="chuck__actions">
+        <button className="chuck__button">generate new joke</button>
+        <Badge badgeContent={4} showZero max={99} color="primary">
+          <SentimentSatisfiedAltIcon sx={{ fontSize: 25 }} />
+        </Badge>
+        <Badge badgeContent={0} showZero max={99} color="primary">
+          <SentimentVeryDissatisfiedIcon sx={{ fontSize: 25 }} />
+        </Badge>
+      </section>
     </main>
   );
 };
